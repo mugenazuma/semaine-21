f973e56e47295f351830d5d56b8109bd37adb547
mugenazuma <77062440+mugenazuma@users.noreply.github.com>
2021-05-27 16:55:07 +0200

    Add files via upload

---
+++ PersonnageDefault.class.php
@@ -0,0 +1,42 @@
+<?php
+class PersonnageDefault
+{
+    public $_nom;
+    public $_prenom;
+    public $_age;
+    public $_sexe;
+
+    public function getNom()
+    {
+        return $this->_nom;
+    }
+    public function setNom()
+    {
+        return $this->_nom = $sNom;
+    }
+    public function getPrenom()
+    {
+        return $this->_prenom;
+    }
+    public function setPrenom()
+    {
+        return $this->_prenom = $sPrenom;
+    }
+    public function getAge()
+    {
+        return $this->_age;
+    }
+    public function setAge()
+    {
+        return $this->_age = $sAge;
+    }
+        public function getSexe()
+    {
+        return $this->_sexe;
+    }
+    public function setSexe() 
+    {
+        return $this->_age = $sAge;
+    }
+    }
+    ?>
